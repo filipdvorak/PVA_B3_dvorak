06214c78a5ebd2c02b68fc756d9475d442b26136
filipdvorak <71071773+filipdvorak@users.noreply.github.com>
2020-11-22 11:49:30 +0100

    Add files via upload

---
+++ Počítání písmen.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+w=input("Zadej větu: ")
+a=e=i=o=u=ě=ý=á=í=é=A=E=I=O=U=Ě=Ý=Á=Í=É=0;
+for x in w:
+    if x=="a": 
+        a+=1
+    if x=="e": 
+        e+=1
+    if x=="i": 
+        i+=1
+    if x=="o": 
+        o+=1
+    if x=="u": 
+        u+=1
+    if x=="ě": 
+        ě+=1
+    if x=="ý": 
+        ý+=1
+    if x=="á": 
+        á+=1
+    if x=="í": 
+        í+=1
+    if x=="é": 
+        é+=1
+    if x=="A": 
+        A+=1
+    if x=="E": 
+        E+=1
+    if x=="I": 
+        I+=1
+    if x=="O": 
+        O+=1
+    if x=="U": 
+        U+=1
+    if x=="Ě": 
+        Ě+=1
+    if x=="Ý": 
+        Ý+=1
+    if x=="Á": 
+        Á+=1
+    if x=="Í": 
+        Í+=1
+    if x=="É": 
+        É+=1
+print ("a=",a)
+print ("e=",e)
+print ("i=",i)
+print ("o=",o)
+print ("u=",u)
+print ("ě=",ě)
+print ("ý=",ý)
+print ("á=",á)
+print ("í=",í)
+print ("é=",é)
+print ("A=",A)
+print ("E=",E)
+print ("I=",I)
+print ("O=",O)
+print ("U=",U)
+print ("Ě=",Ě)
+print ("Ý=",Ý)
+print ("Á=",Á)
+print ("Í=",Í)
+print ("É=",É)
